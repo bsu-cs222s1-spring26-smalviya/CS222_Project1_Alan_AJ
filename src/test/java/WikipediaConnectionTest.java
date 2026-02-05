c913ce651a92609a495e501d2987e47d0ceba2e5
Alan <alan.burns.bsu@gmail.com>
2026-02-05 11:46:01 -0500

    Modified WikipediaConnectionTest - Added wikipediaPageConnectionTest Test Case - Added wikipediaPageReadTest Test Case

--- src/test/java/WikipediaConnectionTest.java
+++ src/test/java/WikipediaConnectionTest.java
@@ -1,4 +1,14 @@
 import org.junit.jupiter.api.Test;
 
 public class WikipediaConnectionTest {
+
+    @Test
+    public void wikipediaPageConnectionTest() {
+
+    }
+
+    @Test
+    public void wikipediaPageReadTest() {
+
+    }
 }
