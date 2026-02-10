c0a96c65edc1b69e601768cef74e3cac38274b2b
Alan <alan.burns.bsu@gmail.com>
2026-02-10 10:57:38 -0500

    Modified WikipediaConnection.java - Added static main() - Added Constructor - Added establishConnectionToWikipedia() - - Added try/catch to contain the exception - Added readJsonAsString() - - Added try/catch to contain the exception

---
+++ src/main/java/ProgramSettings.java
@@ -0,0 +1,5 @@
+public class ProgramSettings {
+
+    public final static String WIKILINK = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=";
+
+}
--- src/main/java/WikipediaConnection.java
+++ src/main/java/WikipediaConnection.java
@@ -1,2 +1,44 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.sql.Connection;
+
 public class WikipediaConnection {
+
+    static void main(String[] args) {
+        WikipediaConnection newConnection = new WikipediaConnection();
+        System.out.println(newConnection.readJsonAsString());
+    }
+
+    private URLConnection connection;
+
+    public WikipediaConnection() {
+        establishConnectionToWikipedia();
+    }
+
+    private void establishConnectionToWikipedia() {
+        try {
+            String encodedUrlString = ProgramSettings.WIKILINK +
+                    URLEncoder.encode("Babbage", Charset.defaultCharset()) + "&rvprop=timestamp" +
+                    URLEncoder.encode("|",Charset.defaultCharset()) + "user&rvlimit=4&redirects";
+            URI uri = new URI(encodedUrlString);
+            connection = uri.toURL().openConnection();
+            connection.setRequestProperty("User-Agent",
+                    "FirstProject/0.1 (academic use; https://example.com)");
+            connection.connect();
+        } catch (IOException | URISyntaxException e) {
+            System.out.println("Error");
+        }
+    }
+
+    public String readJsonAsString() {
+        try {
+            return new String(connection.getInputStream().readAllBytes(), Charset.defaultCharset());
+        } catch(IOException e) {
+            return "Error Retrieving json";
+        }
+    }
 }
