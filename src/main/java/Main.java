85c4d1b65b65ecde17357fd8e8e6c47e6427bb96
Alan <alan.burns.bsu@gmail.com>
2026-02-11 13:33:39 -0500

    Added Console Input with exit sentinel.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     static void main(String[] args) {
+        WikipediaInputConsole console = new WikipediaInputConsole();
+        WikipediaConnection connection = new WikipediaConnection();
 
+        connection.establishConnectionToWikipedia(console.getSearchNameInput());
+        System.out.println(connection.readJsonAsString());
     }
 }
--- src/main/java/WikipediaInputConsole.java
+++ src/main/java/WikipediaInputConsole.java
@@ -8,16 +8,23 @@ public class WikipediaInputConsole {
 
     public String getSearchNameInput() {
         Scanner inputScanner = new Scanner(System.in);
-
+        String input = "";
         boolean running = true;
         while(running) {
-            System.out.print("Enter a name: ");
-            String input = inputScanner.nextLine();
+            System.out.print("Enter a name (Type -1 to exit): ");
+            input = inputScanner.nextLine();
 
-            if(input != null) {
-
+            if(input == null || input == "") {
+                System.out.println("Input cannot be empty.. Try again.");
+            }
+            else if(input.equals("-1")) {
+                System.out.println("Exiting Program..");
+                running = false;
+            }
+            else {
+                running = false;
             }
         }
-        return "";
+        return input;
     }
 }
