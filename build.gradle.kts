e30e63281cebdf3f5ae1b08859f3c0505190269b
ajbeck800 <ajbeck800@gmail.com>
2026-02-16 11:11:56 -0500

    CS222 Project 1 - Added JavaFX to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "com.engine"
@@ -14,8 +16,19 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.json:json:20090211")
+    implementation("org.slf4j:slf4j-nop:2.0.11")
+    implementation("com.jayway.jsonpath:json-path:2.9.0")
+    implementation("net.minidev:json-smart:2.5.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+javafx {
+    version = "22"
+    modules("javafx.controls", "javafx.fxml")
+}
+application {
+    mainClass.set("Main")
 }
